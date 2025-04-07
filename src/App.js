369499b6fef15cb59a5aacb1457819e135113e5a
Alonso Huerta <alonso12_284@outlook.com>
2025-04-07 14:04:29 -0600

    receive token

--- src/App.js
+++ src/App.js
@@ -1,70 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Button from './components/Button';
-import List from './components/List';
-import Add from './components/Add';
-import ResponsiveAppBar from './components/AppBar';
-import LogIn from './pages/LogIn';
-import Home from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Button from "./components/Button";
+import List from "./components/List";
+import Add from "./components/Add";
+import ResponsiveAppBar from "./components/AppBar";
+import LogIn from "./pages/LogIn";
+import Home from "./pages/Home";
 
 function App() {
-	// let [count, setCount] = useState(0);
-	let [items, addItem] = useState([
-		{ id: 1, name: "item1", price: 1 },
-		{ id: 2, name: "item2", price: 2 },
-		{ id: 3, name: "item3", price: 3 },
-	])
-	let add = (name, price) => {
-		let newItems = [...items];
-		newItems.push({ id: newItems.length+1, name: name, price: price });
-		items = addItem(newItems);
-	}
-	let remove = (id) => {
-		let newItems = items.filter(item => item.id !== id);
-		items = addItem(newItems);
-	}
-	let [session, setSession] = useState(null);
-	let login = (username, password) => {
-		console.log("login", username, password);
-		if(username === "admin" && password === "admin"){
-			setSession({ username: username });
-			return true;
-		}
-		else{
-			alert("Invalid username or password");
-			return false;
-		}
-	}
-	let logout = () => {
-		setSession(null);
-	}
+  // let [count, setCount] = useState(0);
+  let [token, setToken] = useState(null);
 
-	
-	return <div>
+  let [items, addItem] = useState([
+    { id: 1, name: "item1", price: 1 },
+    { id: 2, name: "item2", price: 2 },
+    { id: 3, name: "item3", price: 3 },
+  ]);
+  let add = (name, price) => {
+    let newItems = [...items];
+    newItems.push({ id: newItems.length + 1, name: name, price: price });
+    items = addItem(newItems);
+  };
+  let remove = (id) => {
+    let newItems = items.filter((item) => item.id !== id);
+    items = addItem(newItems);
+  };
+  let [session, setSession] = useState(null);
+  let login = async (username, password) => {
+    const result = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await result.json();
+    console.log(data);
+    console.log("login", username, password);
 
-		<BrowserRouter>
-			{ session && <ResponsiveAppBar setLogOut={logout}/>}
-			<Header />
-			<Routes>
-				<Route path="/login" element={<LogIn login={login}/>} />
-				{ session && <Route path="/add" element={<Add add={add} />} /> }
-				{ session && <Route path="/items" element={<List items={items} remove={remove}/>} /> }
-				<Route path="/" element={<Home />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-		{ /* 
+    if (data.operation == true) {
+      setToken(data.token);
+      setSession({ username: username });
+      return true;
+    } else {
+      alert("Invalid username or password");
+      return false;
+    }
+  };
+  let logout = () => {
+    setSession(null);
+  };
+
+  return (
+    <div>
+      <BrowserRouter>
+        {session && <ResponsiveAppBar setLogOut={logout} />}
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LogIn login={login} />} />
+          {session && <Route path="/add" element={<Add add={add} />} />}
+          {session && (
+            <Route
+              path="/items"
+              element={<List items={items} remove={remove} />}
+            />
+          )}
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* 
 		<h1>{count}</h1>
 		<Button name="decrease"	click={() => {count = setCount(count - 1)}}	/>
 		<Button name="increase"	click={() => {count = setCount(count + 1)}}	/>
 		<Button name="mensaje"	click={() => {alert("Hello, world")}}	/>
 		<Add add={add}/> 
-		*/ }
-	</div>
+		*/}
+    </div>
+  );
 }
 
 export default App;
